3e3b98154af99102a3c97f4ee56c1e5fe432a050
Manu <yasermazlum@gmail.com>
2021-12-14 01:37:07 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { ImageCompressService, ResizeOptions, ImageUtilityService, IImage, SourceImage } from 'ng2-image-compress';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'image-comp';
+  title = 'app';
+  selectedImage: any;
+  processedImages: any = [];
+  showTitle: boolean = false;
+
+  constructor(private imgCompressService: ImageCompressService) {
+
+  }
+  onChange(fileInput: any) {
+    //let fileList: FileList;
+    console.warn(fileInput)
+    let images1: Array<IImage> = [];
+
+    ImageCompressService.filesToCompressedImageSource(fileInput.target.files).then(observableImages => {
+      observableImages.subscribe((image) => {
+        images1.push(image);
+        this.processedImages = images1;
+        this.showTitle = true;
+      }, (error) => {
+        console.log("Error while converting");
+      });
+    });
+
+    // or you can pass File[] 
+    let images2: Array<IImage> = [];
+    let files: File[] = Array.from(fileInput.target.files);
+
+    ImageCompressService.filesArrayToCompressedImageSource(files).then(observableImages => {
+      observableImages.subscribe((image) => {
+        images2.push(image);
+      }, (error) => {
+        console.log("Error while converting");
+      }, () => {
+        this.processedImages = images2;
+        this.showTitle = true;
+      });
+    });
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ImageCompressService, ResizeOptions } from 'ng2-image-compress';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ImageCompressService,ResizeOptions],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
