0d2e47a17b1b23cec45ce264f9cfac535599c4c4
Manu <yasermazlum@gmail.com>
2021-12-17 15:11:29 +0300

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { Component, OnInit } from '@angular/core';
 import { ImageCompressService, ResizeOptions, ImageUtilityService, IImage, SourceImage } from 'ng2-image-compress';
 
 @Component({
@@ -10,43 +11,63 @@ import { ImageCompressService, ResizeOptions, ImageUtilityService, IImage, Sourc
 export class AppComponent {
   title = 'app';
   selectedImage: any;
-  processedImages: any = [];
+  processedImages: any;
   showTitle: boolean = false;
+
+  loading: boolean = false;
+
+  fileName: string = "";
+  width: number = 0;
+  height: number = 0;
+  file: any = null;
+  islem: boolean = false;
 
   constructor(private imgCompressService: ImageCompressService) {
 
   }
+
   onChange(fileInput: any) {
-    //let fileList: FileList;
-    console.warn(fileInput)
-    let images1: Array<IImage> = [];
+    this.loading=true;
+    console.log(fileInput)
+    this.file = fileInput.target.files[0];
 
-    ImageCompressService.filesToCompressedImageSource(fileInput.target.files).then(observableImages => {
-      observableImages.subscribe((image) => {
-        images1.push(image);
-        this.processedImages = images1;
-        this.showTitle = true;
-      }, (error) => {
-        console.log("Error while converting");
-      });
-    });
+    const img = new Image();
+    img.src = URL.createObjectURL(fileInput.target.files[0]);
 
-    // or you can pass File[] 
-    let images2: Array<IImage> = [];
-    let files: File[] = Array.from(fileInput.target.files);
-
-    ImageCompressService.filesArrayToCompressedImageSource(files).then(observableImages => {
-      observableImages.subscribe((image) => {
-        images2.push(image);
-      }, (error) => {
-        console.log("Error while converting");
-      }, () => {
-        this.processedImages = images2;
-        this.showTitle = true;
-      });
-    });
-
+    img.onload = () => {
+      console.log("w: " + img.width + " | h: " + img.height)
+      this.width = img.width;
+      this.height = img.height;
+      this.islem = true;
+      if (this.file != null) this.compress(fileInput);
+    };
 
   }
 
+  compress(fileInput: any) {
+    console.log("calisti")
+    let images: Array<ImageData> = [];
+    var option: ResizeOptions = { Resize_Max_Height: this.height, Resize_Max_Width: this.width, Resize_Quality: 50, Resize_Type: "" };
+
+    ImageCompressService.filesToCompressedImageSourceEx(fileInput.target.files, option).then(observableImages => {
+      observableImages.subscribe((image) => {
+        let filename:string= image.fileName;
+        let newData:ImageData={file:image, fileName:filename}
+
+        images.push(newData);
+        
+        this.processedImages = images;
+        this.showTitle = true;
+        this.loading = false;
+        console.log(this.processedImages)
+      }, (error) => {
+        console.log("Error while converting");
+      });
+    });
+  }
+}
+
+export interface ImageData {
+  file: IImage
+  fileName: string
 }
